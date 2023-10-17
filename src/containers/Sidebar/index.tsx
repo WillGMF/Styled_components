2e71e2eec0e031c3a4d1fc95201cd7080f61c380
Willian Gabriel <willian.minikovski@hotmail.com>
2023-10-17 14:56:13 -0300

    estilizando side bar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../components/Avatar'
+import Paragrafo from '../components/Paragrafo'
 import Titulo from '../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/WillGMF.png" />
-    <Titulo fontSize={20}>Willian Gabriel</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Willian Gabriel</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        WillGMF
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Estudante de front-end
+      </Descricao>
+      <BotaoTema> Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
---
+++ src/containers/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/WillGMF.png" />
+
+export default Avatar
